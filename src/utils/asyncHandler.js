4f4d6c7e28b574a16f28ba9aed5a725ef2fb5630
Yutika Nemade <109644250+Yutika-Nemade@users.noreply.github.com>
2025-03-30 17:53:33 +0530

    api

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async() => {}
+
+// using try catch
+// const asyncHandler = (fn) => async(requestAnimationFrame, resizeBy, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }    
+
+
+// using promises
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
